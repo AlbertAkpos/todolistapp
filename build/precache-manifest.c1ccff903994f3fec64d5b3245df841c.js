032d70725b9855eec2694e80e585c0c15452c4ba
AlbertAkpos <47594600+AlbertAkpos@users.noreply.github.com>
2019-08-25 02:18:29 +0100

    Add files via upload

---
+++ build/precache-manifest.c1ccff903994f3fec64d5b3245df841c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091f4ff98026eb4bc86215c32c44864c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44e86651bfc6345ef935",
+    "url": "/static/css/main.a31e0bd7.chunk.css"
+  },
+  {
+    "revision": "b30356aa15cb6d497d4a",
+    "url": "/static/js/2.9448547e.chunk.js"
+  },
+  {
+    "revision": "44e86651bfc6345ef935",
+    "url": "/static/js/main.27a9d18c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
